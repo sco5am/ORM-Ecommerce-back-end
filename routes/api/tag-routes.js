39599ce918a245e780cc9152ce1f0ef533c6be02
Sam Oaks <samuelclayoaks@yahoo.com>
2023-04-09 15:07:51 -0400

    made slight tweeks to category and tag put routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,13 +52,11 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update({
-      where: {
-        id: req.params.id,
-      },
+    const tagData = await Tag.update (req.body,{
+      where: {id: req.params.id}
     });
     if (!tagData) {
-      res.status(404).json({ message: 'No tag found with that id!' });
+      res.status(404).json({ message: 'No tag found with this id!' });
       return;
     }
 
